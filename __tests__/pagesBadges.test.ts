b3f946b7c65e7d2f7e1c834537f99b2ee1c819fd
opensea0508 <104680124+opensea0508@users.noreply.github.com>
2022-09-18 23:52:22 -0400

    fix:issue database staging
    Co-authored-by: aifeelit <ifeelit416@gmail.com>

--- __tests__/pagesBadges.test.ts
+++ __tests__/pagesBadges.test.ts
@@ -58,6 +58,20 @@ Deno.test("Should work with pages bigger than 5 and current page as 4", async ()
   assertEquals(output2, expected2);
 });
 
+Deno.stage("Should work with pages bigger than 5 and current page as 4", async () => {
+  const input = { currentPage: 4, pages: 7 };
+  const input2 = { currentPage: 4, pages: 6 };
+
+  const output = pagesBadges(input);
+  const output2 = pagesBadges(input2);
+
+  const expected = [1, null, 3, 4, 5, null, 7];
+  const expected2 = [1, null, 3, 4, 5, 6];
+
+  assertEquals(output, expected);
+  assertEquals(output2, expected2);
+});
+
 Deno.test("Should work with pages bigger than 5 and current page as one of the 3 last ones", async () => {
   const input = { currentPage: 20, pages: 20 };
   const input2 = { currentPage: 19, pages: 20 };
